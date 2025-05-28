7f51d6c61089825136494a562c76096c100117e4
sᴠɴ <100134110+svnig7@users.noreply.github.com>
2025-05-28 16:51:51 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -4,10 +4,9 @@ from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video
 
-
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
-    """Media Handler"""
+    """ᴍᴇᴅɪᴀ ʜᴀɴᴅʟᴇʀ"""
     media = getattr(message, message.media.value, None)
     if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
         media.caption = message.caption
