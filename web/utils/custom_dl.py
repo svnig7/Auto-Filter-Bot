6365f74c4072c351e7a138041b2cc2690877bd99
sᴠɴ <100134110+svnig7@users.noreply.github.com>
2025-05-28 19:00:45 +0530

    Update custom_dl.py

--- web/utils/custom_dl.py
+++ web/utils/custom_dl.py
@@ -7,15 +7,12 @@ from pyrogram.session import Session, Auth
 from pyrogram.errors import AuthBytesInvalid
 from pyrogram.file_id import FileId, FileType, ThumbnailSource
 
-
 async def chunk_size(length):
     return 2 ** max(min(math.ceil(math.log2(length / 1024)), 10), 2) * 1024
-
 
 async def offset_fix(offset, chunksize):
     offset -= offset % chunksize
     return offset
-
 
 class TGCustomYield:
     def __init__(self):
